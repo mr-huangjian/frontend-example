1ce9326a43389a19f5ac810432ead5bf70547319
mr.huangjian <mr.huangjian@foxmail.com>
2023-04-05 12:33:37 +0800

    m

--- project-webpack/src/index.js
+++ project-webpack/src/index.js
@@ -1,6 +1,10 @@
 import $ from 'jquery'
 
 $(function() {
+    /**
+     * odd 奇数
+     * even 偶数
+     */
     $('li:odd').css('background-color', 'red')
     $('li:even').css('background-color', 'pink')
 })
--- project-webpack/webpack.config.js
+++ project-webpack/webpack.config.js
@@ -1,13 +1,38 @@
 const path = require('path')
-const fs = require('fs')
 
-// https://blog.csdn.net/xinbaiyu/article/details/128814980
+/**
+ * html-webpack-plugin 配置
+ */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+})
+
 
 module.exports = {
-    mode: 'production',
     entry: path.join(__dirname, './src/index.js'),
     output: {
         path: path.join(__dirname, './dist'),
         filename: 'bundle.js'
-    }
+    },
+
+    /**
+     * mode: 'development' 打包更快，开发时使用本模式
+     * mode: 'production' 打包更慢，因为会进行压缩代码等操作
+     */
+    mode: 'development',
+
+    /**
+     * webpack-dev-server 配置
+     * https://blog.csdn.net/feiying0canglang/article/details/126560143
+     */
+    devServer: {
+        port: 2020,
+    },
+
+    /**
+     * 加载三方插件
+     */
+    plugins: [ htmlWebpackPlugin ]
 }
