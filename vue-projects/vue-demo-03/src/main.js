d1022ec8c5f26c9679d08eb28d0f41e99cc4c413
mr.huangjian <mr.huangjian@foxmail.com>
2023-04-08 19:28:09 +0800

    m

--- vue-projects/vue-demo-03/src/main.js
+++ vue-projects/vue-demo-03/src/main.js
@@ -4,5 +4,11 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render: h => h(App),
+})
+
+// 两种挂载方法都可以，将App组件替换掉#app元素
+// new Vue({
+//     render: h => h(App),
+// }).$mount('#app')
