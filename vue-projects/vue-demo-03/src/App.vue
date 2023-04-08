a398a2ab7fdbcd8c374b98f7596e0b0c8705c0e0
mr.huangjian <mr.huangjian@foxmail.com>
2023-04-08 19:38:57 +0800

    m

--- vue-projects/vue-demo-03/src/App.vue
+++ vue-projects/vue-demo-03/src/App.vue
@@ -1,16 +1,21 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
+        <Left></Left>
     </div>
 </template>
 
 <script>
+import Left from "@/components/Left.vue"
 
 export default {
     data() {
         return {
             title: 'App.Vue'
         }
+    },
+    components: {
+        Left,
     }
 }
 </script>
---
+++ vue-projects/vue-demo-03/src/components/Left.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Left</div>
+</template>
