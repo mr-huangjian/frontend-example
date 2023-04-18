031544c21ae01c6feb70b0cf19c30ecc4c941dc3
mr.huangjian <mr.huangjian@foxmail.com>
2023-04-19 00:29:44 +0800

    m

--- vue-projects/vue-router-02/src/App.vue
+++ vue-projects/vue-router-02/src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-container">
+    <h2>App 组件</h2>
+
+    <a href="#/home">首页</a>
+    <a href="#/movie">电影</a>
+    <a href="#/about">关于</a>
+
+    <hr>
+
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less" scoped>
+.app-container {
+  background-color: #efefef;
 }
 </style>
---
+++ vue-projects/vue-router-02/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '@/components/Home.vue'
+import Movie from '@/components/Movie.vue'
+import About from '@/components/About.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/movie',
+      component: Movie
+    },
+    {
+      path: '/about',
+      component: About
+    }
+  ]
+})
+
+export default router
